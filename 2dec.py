aaa95002fc3e0d2ec573614e04d94dbe4d08baca
Shailendra Jain <shailendrajain28@gmail.com>
2024-12-03 10:17:41 +0000

    2 dec part 2

--- 2dec.py
+++ 2dec.py
@@ -1,25 +1,15 @@
-def chceckForIncreasingOrder(report):
-    for i, val in enumerate(report):
-        if i > 0 and (val - report[i - 1] == 0 or val - report[i - 1] > 3 or val - report[i - 1] < 0):
-            return 0
-    return 1
+def is_safe(report):
+    # return true if safe otherwise false
+    decreasing = all([1<= report[i] - report[i+1] <= 3 for i in range(len(report)-1)])
+    increasing = all([1<= report[i+1] - report[i] <= 3 for i in range(len(report)-1)])
+    return increasing or decreasing
 
-
-def checkForDecreasingOrder(report):
-    for i, val in enumerate(report):
-        if i > 0 and (report[i - 1] - val == 0 or report[i - 1] - val > 3 or val - report[i - 1] > 0):
-            return 0
-    return 1
-
-
-def reports_safe(report):
-    # return 1 if report is safe otherwise 0
-    if report[1] > report[0]:
-        resp = chceckForIncreasingOrder(report)
-    else:
-        resp = checkForDecreasingOrder(report)
-    return resp
-
+def can_be_safe_with_dampner(report):
+    for i in range(len(report)):
+        modified_report = report[:i]+report[i+1:]
+        if is_safe(modified_report):
+            return True
+    return False
 
 # 7 6 4 2 1
 # 1 2 7 8 9
@@ -47,7 +37,7 @@ import os
 
 def main():
     # Reading the uploaded file into two lists: one for the first column and one for the second column.
-    file_path = os.path.join(os.path.dirname(__file__), '2.txt')
+    file_path = os.path.join(os.path.dirname(__file__), 'data/2.txt')
 
     # Initialize two lists to hold the columns
     reports_safe_count = 0
@@ -57,7 +47,8 @@ def main():
         for line in file:
             if line.strip():  # Skip empty lines
                 report = list(map(int, line.split()))
-                reports_safe_count += reports_safe(report)
+                if is_safe(report) or can_be_safe_with_dampner(report):
+                    reports_safe_count += 1
 
     return reports_safe_count
 
